cfe55aaa0e4f111c0b54045c0ac12eda002f31d8
Kenya Womack <ladykamillion27@gmail.com>
2023-03-07 04:09:27 -0600

    updated new page

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # all-about-me
 class 201 lab 2
+
+Kenya Womack
+
+Screenshot (2).png
---
+++ app.js
@@ -0,0 +1,51 @@
+'use strict';
+
+let userName = prompt('What is your name?').toLowerCase();
+console.log(userName);
+
+alert('Hello,' + userName);
+
+let myAge =prompt('Am I 42? Type yes or no').toLowerCase();
+console.log(myAge); 
+if (myAge === 'yes') {
+  alert(username + 'You are incorrect. I just had a birthday and I am 43!');
+
+} else if (myAge === 'no') {
+  let myAge = prompt('You are correct! I am 43!').toLowerCase();
+}
+
+let QuestionTwo =prompt('Do I like dogs? Type yes or no').toLowerCase();
+console.log(QuestionTwo); 
+if (QuestionTwo === 'yes') {
+  alert(userName + 'You are correct!');
+
+} else if (QuestionTwo === 'no') {
+  let QuestionTwo = prompt('You are not correct!');
+}
+
+let QuestionThree =prompt('Do I like pink? Type yes or no').toLowerCase();
+console.log(QuestionThree); 
+if (QuestionThree === 'yes') {
+  alert(userName + 'You are correct!');
+
+} else if (QuestionThree === 'no') {
+  let QuestionThree = prompt('You are not correct!');
+}
+
+let QuestionFour =prompt('Do I like pink? Type yes or no').toLowerCase();
+console.log(QuestionFour); 
+if (QuestionFour === 'yes') {
+  alert(userName + 'You are correct!');
+
+} else if (QuestionFour === 'no') {
+  let QuestionFour = prompt('You are not correct!');
+}
+
+let QuestionFive =prompt('Do I like computer scuence? Type yes or no').toLowerCase();
+console.log(QuestionFive); 
+if (QuestionFive === 'yes') {
+  alert(userName + 'You are correct!');
+
+} else if (QuestionFour === 'no') {
+  let QuestionFive = prompt('You are not correct!');
+}
